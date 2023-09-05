b4205d121e524d429a76c2e1ecdc8f59a11f09c5
bethjm <b.jeanne.mills@gmail.com>
2023-09-05 14:06:51 -0400

    adding project

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -1,15 +1,20 @@
-import { View, Text } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello, Hello</Text>
+    <View style={styles.container}>
+      <Text>Hello World!!!!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
