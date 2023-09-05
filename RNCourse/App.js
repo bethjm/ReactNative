23d753b51e94ffa70a00a018d2acdf90182d0f75
bethjm <b.jeanne.mills@gmail.com>
2023-09-05 14:47:45 -0400

    added button

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello, Beth</Text>
-      <StatusBar style="auto" />
+      <Text>Hello, Beth....</Text>
+      <Text>Hello again, I guess</Text>
+      <Button title="Click me" />
     </View>
   );
 }
