a4090c96e4a9721cb07cf1c99ea68dda92ef04ce
bethjm <b.jeanne.mills@gmail.com>
2023-09-11 12:36:14 -0400

    added image

--- Guess_Number/screens/GameOverScreen.js
+++ Guess_Number/screens/GameOverScreen.js
@@ -1,11 +1,35 @@
-import { Text } from "react-native";
+import { Text, View, Image, StyleSheet } from "react-native";
+import Title from "../components/ui/Title";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 function GameOverScreen() {
   return (
-    <Text>
-      <Text>Game Is Over</Text>
-    </Text>
+    <View>
+      <Title>GAME OVER!</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/success.png")}
+        />
+      </View>
+    </View>
   );
 }
 
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    width: 400,
+    height: 400,
+    borderRadius: 200,
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    overflow: "hidden",
+    margin: 36,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
